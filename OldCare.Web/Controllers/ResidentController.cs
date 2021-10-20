6e9f9cad8f87e82f94155f52dc1b8e23c902083b
Abiratan Lopes de Sousa <abiratan@yahoo.com.br>
2021-10-19 22:15:54 -0300

    Criação parcial de Resident.

---
+++ OldCare.Web/Controllers/ResidentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OldCare.Core.Data;
+using System.Threading.Tasks;
+
+namespace OldCare.Web.Controllers
+{
+    public class ResidentController : Controller
+    {
+        private OldCareDataContext _context;
+
+        public ResidentController(OldCareDataContext context) => _context = context;    
+        
+        public async Task<IActionResult> Index() 
+        {
+
+            var model = await _context.Persons.ToListAsync();
+
+            return View(model);
+        }
+    }
+}
