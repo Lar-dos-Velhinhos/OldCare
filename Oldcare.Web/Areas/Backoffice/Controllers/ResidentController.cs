2b77a50b9bceb2461507de07573c3ca0a57a70ce
Brewerton Santos <brewerton.santos@gmail.com>
2021-10-30 20:14:12 -0300

    Crated person card partial view.

---
+++ Oldcare.Web/Areas/Backoffice/Controllers/ResidentController.cs
@@ -0,0 +1,65 @@
+﻿namespace Oldcare.Web.Areas.Backoffice.Controllers;
+
+[Area("Backoffice")]
+public class ResidentController : Controller
+{
+    // GET: ResidentController
+    public ActionResult Index() => View();
+
+    // GET: ResidentController/Details/5
+    public ActionResult Details(int id) => View();
+
+    // GET: ResidentController/Create
+    public ActionResult Create() => View();
+
+    // POST: ResidentController/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+
+    // GET: ResidentController/Edit/5
+    public ActionResult Edit(int id) => View(id);
+
+    // POST: ResidentController/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(int id, IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+
+    // GET: ResidentController/Delete/5
+    public ActionResult Delete(int id) => View(id);
+
+    // POST: ResidentController/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Delete(int id, IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+}
