5d83e861a1651861c2aa2b62cda6c30e6e7b24f9
Brewerton Santos <brewerton.santos@gmail.com>
2021-11-03 19:45:25 -0300

    Added create person viewmodels.

---
+++ Oldcare.Core/ViewModels/Backoffice/Person/CreateStartPersonViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Oldcare.Core.ViewModels.Backoffice.Person;
+
+public class CreateStartPersonViewModel
+{
+    public Guid Id { get; set; }
+    
+    [Display(Name = "Nome")]
+    [MaxLength(255, ErrorMessage = "O campo \'nome\' da pessoa precisa conter um máximo de 255 caracteres.")]
+    [MinLength(2, ErrorMessage = "O campo \'nome\' da pessoa precisa conter ao menos 2 letras.")]
+    [Required(ErrorMessage = "O campo \'nome\' não pode estar vazio.")]
+    public string Name { get; set; }
+    
+    [Display(Name = "Data de nascimento")]
+    public DateTime? BirthDate { get; set; }
+    
+    [Display(Name = "Naturalidade")]
+    public string? Citizenship { get; set; }
+    
+    [Display(Name = "Gênero")]
+    [Required(ErrorMessage = "Uma opção válida de \'gênero\' deve ser selecionada.")]
+    public bool Gender { get; set; }
+    
+    [Display(Name = "RG")]
+    public long? RG { get; set; }
+    
+    [Display(Name = "CPF")]
+    public long? CPF { get; set; }
+}
