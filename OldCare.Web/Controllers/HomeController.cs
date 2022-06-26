cdecb00f7e204a968c2482ae0fe1f2f373f650af
Brewerton <brewerton.santos@gmail.com>
2022-06-26 09:59:03 -0300

    Adição de novas partials à página pública.

--- OldCare.Web/Controllers/HomeController.cs
+++ OldCare.Web/Controllers/HomeController.cs
@@ -1,23 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Balta.Web.Controllers;
+namespace OldCare.Web.Controllers;
 
 public class HomeController : Controller
 {
-    [HttpGet("politicas/cancelamento")]
-    public IActionResult CancelPolicy() => View();
-    
     public IActionResult Index() => View();
-    
-    [HttpGet("como-funciona")]
-    public IActionResult HowWorks() => View();
-
-    [HttpGet("politicas/privacidade")]
-    public IActionResult PrivacyPolicy() => View();
-
-    [HttpGet("social")]
-    public IActionResult Social() => View();
-
-    [HttpGet("politicas/uso")]
-    public IActionResult UsePolicy() => View();
 }
